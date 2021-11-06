61c6bda28d76d303517cc1fddf5bf0dbb688b43a
adityakrishnan05 <87619862+adityakrishnan05@users.noreply.github.com>
2021-11-05 21:15:41 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,150 +1,64 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
+var backgroundImg;
+var hour;
 
-var particle;
-var divisions = [];
-var particles = [particle];
-var plinkos = [];
-var line;
+var bg = "sunrise1.png";
 
-var divisionHeight = 300;
+function preload() {
+    // create getBackgroundImg( ) here
+    getBackgroundImg();
+}
 
-var gameState = "PLAY";
-
-var count = 0;
-var score = 0;
-
-function setup() {
-    createCanvas(800, 800);
+function setup(){
+    var canvas = createCanvas(1200,700);
     engine = Engine.create();
     world = engine.world;
-    ground = new Ground(width/2,height,width,20);
-  
-  
-     for (var k = 0; k <=width; k = k + 80) {
-       divisions.push(new Division(k, height-divisionHeight/2, 10, divisionHeight));
-     }
-  
-  
-      for (var j = 75; j <=width; j=j+50) 
-      {
-      
-         plinkos.push(new Plinko(j,75));
-      }
-  
-      for (var j = 50; j <=width-10; j=j+50) 
-      {
-      
-         plinkos.push(new Plinko(j,175));
-      }
-  
-       for (var j = 75; j <=width; j=j+50) 
-      {
-      
-         plinkos.push(new Plinko(j,275));
-      }
-  
-       for (var j = 50; j <=width-10; j=j+50) 
-      {
-      
-         plinkos.push(new Plinko(j,375));
-      }
-  
-      
-  
-      
-  }
 
- 
+}
 
-function draw() {
-    background(0);
+function draw(){
+     // add condition to check if any background image is there to add
+    if(backgroundImg)
+    background(backgroundImg);
+
     Engine.update(engine);
-    textSize(35)
-  text("Score : "+score,20,40);
-  fill("red");
-  
-  textSize(35)
-  text(" 500 ", 5, 550);
-  text(" 500 ", 80, 550);
-  text(" 500 ", 160, 550);
-  text(" 500 ", 240, 550);
-  text(" 100 ", 320, 550);
-  text(" 100 ", 400, 550);
-  text(" 100 ", 480, 550);
-  text(" 200 ", 560, 550);
-  text(" 200 ", 640, 550);
-  text(" 200 ", 720, 550);
-  
+    // write code to display time in correct format here
+    fill("black");
+    textSize(30);
 
+    if(hour>=12){
+        text("Time : "+ hour%12 + " PM", 50,100);
+       }else if(hour==0){
+         text("Time : 12 AM",100,100);
+       }else{
+        text("Time : "+ hour%12 + " AM", 50,100);
+       }
 
-
-    ground.display();
-    if ( gameState =="END") {
-        background("black");
-        fill("red");
-        textSize(100);
-        text("Game Over", 200, 400);
-       
-      } 
-      for(var k = 0; k < plinkos.length; k++) {
-       plinkos[k].display();
-    }
-
-    if(particle!=null)
-    {
-       particle.display();
-        
-        if (particle.body.position.y>700)
-        {
-              if (particle.body.position.x < 300) 
-              {
-                  score=score+500;      
-                  particle=null;
-                  if ( count>= 5) gameState ="END";                          
-              }
-
-
-              else if (particle.body.position.x < 600 && particle.body.position.x > 301 ) 
-              {
-                    score = score + 100;
-                    particle=null;
-                    if ( count>= 5) gameState ="END";
-
-              }
-              else if (particle.body.position.x < 900 && particle.body.position.x > 601 )
-              {
-                    score = score + 200;
-                    particle=null;
-                    if ( count>= 5)  gameState ="END";
-
-              }      
-              
-        }
-    }
-    
-
-   
-    
-  
- for (var i = 0; i < divisions.length; i++) {
-     
-    divisions[i].display();
-  }
-  
 }
 
+async function getBackgroundImg(){
 
-function mousePressed() {
-    if(gameState !== "END") {
-        count++;
-    particle = new Particle(mouseX, 50, 10, 10);
+    // write code to fetch time from API
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+
+    //change the data in JSON format
+    var responseJSON = await response.json();
+    var datetime = responseJSON.datetime;
+    
+    // write code slice the datetime
+    hour = datetime.slice(11,13);
+
+    if(hour>=0 && hour<18 ){
+        bg = "sunrise.png";
     }
+    else{
+        bg="sunset.png"
+    }
+    
+    backgroundImg = loadImage(bg);
 }
-
-
-
-
